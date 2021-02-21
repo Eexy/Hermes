d7dc98904bed41d051ab64afa1b4c41c8c791613
unknown <eexdhe@gmail.com>
2021-02-21 17:31:53 +0100

    create function to join room and get the messages and user

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -1,18 +1,64 @@
-let socket = null;
-const me = {id: null, room: null};
-let name = getUserName();
+let socket = new io();
+const me = { id: null, name: null, dest: null };
 
-function getUserName(){
-  let name = prompt('Enter your name please: ');
-  if(name != null){
-    socket = new io();
-    me.id = socket.id;
-    return name;
+const form = document.querySelector('#form');
+const messageInput = document.querySelector('#message-input');
+
+function getUserName() {
+  let name = prompt("Enter your name please: ");
+  if (name != null) {
+    me.name = name;
+    socket.emit("user connect", name);
   }
 }
 
-socket.on('update info', updateInfo)
+socket.on("connected", (id) => (me.id = id));
 
-function updateInfo(update){
-  console.log(update);
+window.addEventListener("DOMContentLoaded", getUserName);
+
+socket.on("join room", (room) => {
+  me.dest = room.name;
+  getUsersList(room.users);
+  getMessages(room.messages);
+});
+
+function getUsersList(users) {
+  const usersList = document.querySelector(".users__list");
+
+  // we delete all the user
+  while (usersList.lastChild) {
+    usersList.removeChild(usersList.lastChild);
+  }
+
+  // we create the new user list
+  users.forEach((user) => {
+    const li = document.createElement("li");
+    li.classList.add("user");
+    li.dataset.userId = user.id;
+
+    li.textContent = `${user.name}`;
+    usersList.appendChild(li);
+  });
 }
+
+function getMessages(messages) {
+  const messagesList = document.querySelector(".messages");
+
+  // we delete all the messages
+  while (messagesList.lastChild) {
+    messagesList.removeChild(messagesList.lastChild);
+  }
+
+  messages.forEach((message) => {
+    const li = document.createElement("li");
+    li.classList.add("message");
+
+    li.textContent = `${message.message}`;
+    messagesList.appendChild(li);
+  });
+}
+
+socket.on("user join", (users) => {
+  console.log(users);
+  getUsersList(users)
+});
--- src/server/io.js
+++ src/server/io.js
@@ -7,19 +7,40 @@ let users = [];
 let rooms = new Map();
 
 // setting general rooms
-rooms.set('general', {users: [], message: []})
+rooms.set('general', {name: 'general', users: [], messages: []})
 
-io.on("connection", async (socket) => {
-  const userId = socket.id;
+function connect(name, socket){
   // add the user to the list of users
-  users.push({id: userId, room: 'general'});
+  const user = {id: socket.id, name, room: 'general'};
+  users.push(user);
 
-  // push the user in the room
+  // We send back his socket id
+  socket.join(socket.id);
+  io.to(socket.id).emit('connected', socket.id)
+
+  // Add user to the general room
+  joinRoom('general', user, socket);
+}
+
+function joinRoom(room, user, socket){
+  // we inform all the user that someone enter the room
+  // we send them the new list of user
+  io.to(room).emit('user join', [user, ...rooms.get(room).users]);
+
+  // We had the new user to the room
+  let value = rooms.get(room);
+  value.users.push(user);
+  rooms.set(room, value);
+  
+  // We sent back to the user his new room
+  // and connect it's socket to the room
+  socket.join(socket.id);
   socket.join('general');
+  io.to(socket.id).emit('join room', rooms.get(room));
+}
 
-  // send back the room to the users
-  socket.join(userId);
-  io.to(userId).emit('update info', {room: 'general'});
+io.on("connection", (socket) => {
+  socket.on('user connect', (name) => connect(name, socket));
 });
 
 http.listen(PORT, () => console.log(`Chat server listening port ${PORT}`));
